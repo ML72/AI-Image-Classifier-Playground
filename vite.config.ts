f4ef7f8e50dda2b8b892d24e4accc5464b8323ca
Michael <michael.mingdao.li@gmail.com>
2025-12-01 14:23:16 -0500

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI-Image-Classifier-Playground/',
   server: {
     port: 3000,
     open: true,
